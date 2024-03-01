932b6fb2a86fec1e7f46aceeaa01c9df5c157047
RaffyJO <rafiteguh6@gmail.com>
2024-03-01 09:17:38 +0700

    feat: lab exercise report

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserController;
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductsController::class, 'foodBeverage']);
